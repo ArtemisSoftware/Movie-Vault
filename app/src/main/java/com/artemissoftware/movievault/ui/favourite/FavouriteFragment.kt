f92233cb0beadf9daec0de088328b3564a100ca2
gustavo <gustavo.maia@b2b.com.pt>
2020-12-07 16:11:43 +0000

    Navigation & Fragment

---
+++ app/src/main/java/com/artemissoftware/movievault/ui/favourite/FavouriteFragment.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.movievault.ui.favourite
+
+import androidx.fragment.app.Fragment
+import com.artemissoftware.movievault.R
+
+class FavouriteFragment : Fragment(R.layout.fragment_favourite){
+}
---
+++ app/src/main/java/com/artemissoftware/movievault/ui/movie/MovieFragment.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.movievault.ui.movie
+
+import androidx.fragment.app.Fragment
+import com.artemissoftware.movievault.R
+
+class MovieFragment : Fragment(R.layout.fragment_movie){
+}
